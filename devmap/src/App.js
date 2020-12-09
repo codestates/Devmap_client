af9960051c10b0c47f32b3413893aa4fc54afe3a
임희찬 <imhuichan@imhuichan-ui-MacBookPro.local>
2020-12-09 16:09:26 +0900

    feat/ start1/

--- devmap/src/App.js
+++ devmap/src/App.js
@@ -1,26 +1,22 @@
 import logo from './logo.svg';
 //import './App.css';
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import axios from 'axios';
 import { Switch, Route, Redirect } from "react-router-dom";
 import styled from 'styled-components';
+import Top from './pages/Top'
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: black;
+  border-radius: 50%;
+`;
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Circle />
+      <Top></Top>
     </div>
   );
 }
---
+++ devmap/src/pages/Top.jsx
@@ -0,0 +1,12 @@
+
+import React, { useState , useEffect} from 'react';
+import { Switch, Route, Redirect } from "react-router-dom";
+import styled from 'styled-components';
+function Top (){
+    return (
+        <>
+            <span>hi</span>
+        </>
+    );
+}
+export default Top;
