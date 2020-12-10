20dcf313628cd1b8efd51b60d4d0e96ad0b519e7
임희찬 <imhuichan@imhuichan-ui-MacBookPro.local>
2020-12-10 17:33:54 +0900

    ADD/ #23 / TOP-Style

--- devmap/src/App.js
+++ devmap/src/App.js
@@ -3,23 +3,23 @@ import { Link, Switch, Route, Redirect } from "react-router-dom";
 import axios from 'axios';
 import styled from 'styled-components';
 
-//import './App.css';
+import './App.css';
 import Top from './pages/Top'
 
 function App() {
   const [isSignnedIn, setIsSignnedIn] = useState(false);
   const [userInfo, setUserInfo] = useState(null);
 
-  componentDidMount(); // 로그인 여부 판단하여 첫 렌더 화면 결정
+  //componentDidMount(); // 로그인 여부 판단하여 첫 렌더 화면 결정
 
-  handleResponseSuccess = async () => { // 로그인 여부 판단
+  //handleResponseSuccess = async () => { // 로그인 여부 판단
 
-  };
+  //};
 
   return (
     <div className="App">
       <header>
-        <Top isSignnedIn={isSignnedIn} userInfo={userInfo} handleResponseSuccess={handleResponseSuccess}></Top>
+        <Top isSignnedIn={isSignnedIn} userInfo={userInfo}></Top>
       </header>
     </div>
   );
