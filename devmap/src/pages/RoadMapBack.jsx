6bb2fbe90db518b3d5db32faeb486c1ecc9c7d1e
intothejourneys <intothejourneys@gmail.com>
2020-12-14 08:05:29 +0900

    feature/roadmapsetting

--- devmap/src/pages/RoadMapBack.jsx
+++ devmap/src/pages/RoadMapBack.jsx
@@ -1,1 +1,20 @@
- 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import { darken, lighten } from 'polished';
+
+export default function RoadMapBack() {
+    const RoadMapWrapper = styled.div`
+        width: 97%;
+        height: 755px;
+        margin: 0 auto;
+        background-color: #eefffe;
+        border: 5px solid;
+        border-radius: 10px;
+        border-color: #a3dfea;
+    `;
+
+    return (
+        <RoadMapWrapper>백엔드 로드맵</RoadMapWrapper>
+    )
+}
