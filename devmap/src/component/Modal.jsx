f9c5f080f8bc4604262f90731208441cf432b403
intothejourneys <intothejourneys@gmail.com>
2020-12-26 10:24:31 +0900

    feature/signup/idcheck/iderase

---
+++ devmap/src/component/Modal.jsx
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react';
+
+// import SignUpModal from './SignUpModal';
+// import SignInModal from './SignInModal';
+
+// class Modal extends Component {
+//     state = {
+//         token: '', 
+//         id: null
+//     }
+
+//     updateUserInfo = () => {
+//         this.setState({token: data.token, id: data.user.id})
+//         console.log('data', data)
+//     };
+
+//     render() {
+//         return (
+//             <div>
+//                 <SignInModal updateUserInfo={this.updateUserInfo} />
+//                 <SignUpModal updateUserInfo={this.updateUserInfo} />
+//                 {console.log(this.updateUserInfo)}
+//             </div>
+//         )
+//     }
+// }
